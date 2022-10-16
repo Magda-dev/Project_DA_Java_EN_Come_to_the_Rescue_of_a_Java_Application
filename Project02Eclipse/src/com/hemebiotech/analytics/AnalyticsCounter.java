83af6742802fdb79a3184cf7c8cdca9c57c85a7c
magda_dev <30729081+Magda-dev@users.noreply.github.com>
2022-10-17 01:54:26 +0200

    final cleaning

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -18,49 +18,6 @@ public class AnalyticsCounter {
 
 		//Exporter les symptomes
 		readSymptomData.exportSymptoms((HashMap<String, Integer>)symptomsAnalytics);
-		//donner un nom de fichier en parametre (à formatter)
 
-
-
-
-
-
-
-
-
-
-		HashMap<String, Integer> symptomsMap = new HashMap<>();
-
-/**
-		int i = 0;    // set i to 0
-		int headCount = 0;    // counts headaches
-		Symptom symptom = null;
-		while (line != null) {
-
-			i++;    // increment i
-			//System.out.println("symptom from file: " + line);
-			if (symptomsMap.containsKey(line)) {
-				symptomsMap.get(line);
-				symptomsMap.merge(line, 1, Integer::sum);
-
-
-			} else {
-				symptom = new Symptom(line, 1);
-
-				symptomsMap.put(line, symptomCounter);
-
-			}
-			line = reader.readLine();    // get another symptom
-
-		}
-		System.out.println(symptomsMap);
-		// next generate output
-		FileWriter writer = new FileWriter("result.out");
-
-		writer.write(symptom + " : " + String.valueOf(symptomCounter));
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}*/
 }
 }
